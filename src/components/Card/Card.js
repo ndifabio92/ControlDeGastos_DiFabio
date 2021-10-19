58f73e3910e1a237797b559b9b610ca90a841e45
Nicolas Di Fabio <nicolasdifabio92@gmail.com>
2021-10-18 23:08:35 -0300

    Se agrega la funcionalidad a la hora de sumar o restar cantidad de productos

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-
 import { ShCartIcon, VisIcon } from '../Icons/ListIcons';
 import '../styles/card.css';
 
-export const ImgMediaCard = ({ alt, url, name }) => {
+export const ImgMediaCard = ({ alt, url, name, stock = 0, initial }) => {
+    const [ counter, setCounter ] = useState( initial );
+    const [ isActiveAdd, setIsActiveAdd ] = useState();
+    const [ isActiveRest, setIsActiveRest ] = useState();
+
+    useEffect(() => {
+        handleActiveAdd();
+        handleActiveRest();
+    }, [ counter ] )
+
+    const handleAdd = () => ( counter < stock ) && setCounter( counter + 1 );
+    const handleRest = () => ( counter !== 1 ) && setCounter( counter - 1 );
+
+    const handleActiveAdd = () => ( counter === stock ? setIsActiveAdd( true ) : setIsActiveAdd( false ));
+    const handleActiveRest = () => ( counter === initial ? setIsActiveRest( true ) : setIsActiveRest( false ));
+
     return (
         <Card sx={{ maxWidth: 345 }} className="card">
             <CardMedia
@@ -22,6 +36,15 @@ export const ImgMediaCard = ({ alt, url, name }) => {
                 <Typography gutterBottom variant="h5" component="div">
                     { name }
                 </Typography>
+                <div className="card-content-actions">
+                    <Button variant="outlined" color="success" size="small" onClick= { handleAdd } disabled= { isActiveAdd }> 
+                        + 
+                    </Button>
+                    <h4> { counter } </h4>
+                    <Button variant="outlined" color="error" size="small" onClick= { handleRest } disabled= { isActiveRest }>
+                        - 
+                    </Button>
+                </div>
             </CardContent>
             <CardActions className="card-buttons">
                 <Button size="small"><VisIcon /> Detalle</Button>
--- src/components/ListCards.js
+++ src/components/ListCards.js
@@ -4,7 +4,8 @@ import { ImgMediaCard } from './Card/Card';
 export const ListCards = () => {
     return (
         <div className="container">
-            <ImgMediaCard alt= 'pendrive' url= '/assets/images/pendrive.png' name= 'Pendrive'/>
+            <ImgMediaCard alt= 'pendrive' url= '/assets/images/pendrive.png' name= 'Pendrive' stock={ 10 } initial= { 1 } />
+            <ImgMediaCard alt= 'pendrive' url= '/assets/images/pendrive.png' name= 'Pendrive' stock={ 1 } initial= { 1 } />
         </div>
     )
 }
