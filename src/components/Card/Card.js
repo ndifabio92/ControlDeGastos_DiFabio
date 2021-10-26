ad1d0a4a91817e9cf8a98710b636f37f66ee62d5
Nicolas Di Fabio <nicolasdifabio92@gmail.com>
2021-10-26 00:01:41 -0300

    se genera el detalle del producto

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -6,43 +6,52 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { ShCartIcon, VisIcon } from '../Icons/ListIcons';
-import '../styles/card.css';
 import { SwalAlert } from '../Alerts/SwalAlert';
-
+import { CardModal } from './CardModal';
+import '../styles/card.css';
 
 export const ImgMediaCard = ({ alt, url, name, stock = 0, initial, info }) => {
 
     const [ counter, setCounter ] = useState( initial );
+    const [ open, setOpen ] = useState( false );
+
+    const handleOpen = () => setOpen( true );
+    const handleClose = () => setOpen( false );
 
     const handleAdd = () => ( counter < stock ) ? setCounter( counter + 1 ) : SwalAlert( 'Alerta', 'La cantidad seleccionada no puede ser mayor al stock del articulo.', 'warning' );
     const handleRest = () => ( counter !== initial ) ? setCounter( counter - 1 ) : SwalAlert( 'Alerta', 'La cantidad seleccionada no puede ser menor al stock del articulo.', 'warning' );
 
     return (
-        <Card sx={{ maxWidth: 345 }} className="card">
-            <CardMedia
-                component="img"
-                alt = { alt }
-                height="140"
-                image= { url }
-            />
-            <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                    { name }
-                </Typography>
-                <div className="card-content-actions">
-                    <Button variant="outlined" color="success" size="small" onClick= { handleAdd } > 
-                        + 
-                    </Button>
-                    <h4> { counter } </h4>
-                    <Button variant="outlined" color="error" size="small" onClick= { handleRest } >
-                        - 
-                    </Button>
-                </div>
-            </CardContent>
-            <CardActions className="card-buttons">
-                <Button size="small"><VisIcon /> Detalle</Button>
-                <Button size="small"><ShCartIcon /> Agregar</Button>
-            </CardActions>
-        </Card>
+        <>
+            <Card sx={{ width: 345 }} className="card">
+                <CardMedia
+                    className=" card-img "
+                    component="img"
+                    alt = { alt }
+                    height="140"
+                    image= { url }
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div" className="card-title">
+                        { name }
+                    </Typography>
+                    <div className="card-content-actions">
+                        <Button variant="outlined" color="success" size="small" onClick= { handleAdd } > 
+                            + 
+                        </Button>
+                        <h4> { counter } </h4>
+                        <Button variant="outlined" color="error" size="small" onClick= { handleRest } >
+                            - 
+                        </Button>
+                    </div>
+                </CardContent>
+                <CardActions className="card-buttons">
+                    <Button size="small" onClick={ handleOpen } ><VisIcon /> Detalle</Button>
+                    <Button size="small"><ShCartIcon /> Agregar</Button>
+                </CardActions>
+            </Card>
+
+            <CardModal open={ open } handleClose={ handleClose } name={ name } url={ url } alt={ alt } info={ info }/>
+        </>
     )
 }
